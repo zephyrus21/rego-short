6482b754fdc0e1a66cf89af9e71b544fb6652c2f
zephyrus21 <pandey.piyus@gmail.com>
2022-07-14 16:22:41 +0530

    added reolvers and shorten apis

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,7 +1,32 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/go-redis/redis/v9"
+	"github.com/gofiber/fiber/v2"
+	"github.com/zephyrus21/rego-short/database"
+)
 
 func ResolveURL(c *fiber.Ctx) error {
-	return nil
+	url := c.Params("url")
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	val, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "short not found in DB",
+		})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "cannot connect to DB",
+		})
+	}
+
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+
+	_ = rInr.Incr(database.Ctx, "COUNTER")
+
+	return c.Redirect(val, 301)
 }
--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,10 +1,15 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/asaskevich/govalidator"
+	"github.com/go-redis/redis/v9"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/internal/uuid"
+	"github.com/zephyrus21/rego-short/database"
 	"github.com/zephyrus21/rego-short/helpers"
 )
 
@@ -32,6 +37,21 @@ func ShortenURL(c *fiber.Ctx) error {
 	}
 
 	//? implement rate limiting
+	r2 := database.CreateClient(1) //# redis client
+	defer r2.Close()
+	val, err := r2.Get(database.Ctx, c.IP()).Result()
+	if err == redis.Nil {
+		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+		valInt, _ := strconv.Atoi(val)
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error":           "Rate limit exceeded",
+				"rate_limit_rest": limit / time.Nanosecond / time.Minute,
+			})
+		}
+	}
 
 	//? check if the input URL is valid
 	if !govalidator.IsURL(body.URL) {
@@ -50,5 +70,52 @@ func ShortenURL(c *fiber.Ctx) error {
 	//? enforce https
 	body.URL = helpers.EnforceHTTP(body.URL)
 
-	return nil
+	//? check if the input URL is already exists
+	var id string
+	if body.CustomShort == "" {
+		id = uuid.NewV4().String()[:6]
+	} else {
+		id = body.CustomShort
+	}
+
+	r := database.CreateClient(0) //# redis client
+	defer r.Close()
+
+	val, _ = r.Get(database.Ctx, id).Result()
+	if val != "" {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"error": "Short URL already exists",
+		})
+	}
+
+	if body.Expiry == 0 {
+		body.Expiry = time.Hour * 24 * 365
+	}
+
+	err = r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Unable to connect to the server",
+		})
+	}
+
+	resp := response{
+		URL:                 body.URL,
+		CustomShort:         "",
+		Expiry:              body.Expiry,
+		XRateLimitRemaining: 10,
+		XRateLimitRest:      30,
+	}
+
+	r2.Decr(database.Ctx, c.IP())
+
+	val, _ = r2.Get(database.Ctx, c.IP()).Result()
+	resp.XRateLimitRemaining, _ = strconv.Atoi(val)
+
+	ttl, _ := r2.TTL(database.Ctx, c.IP()).Result()
+	resp.XRateLimitRest = ttl / time.Nanosecond / time.Minute
+
+	resp.CustomShort = os.Getenv("DOMAIN") + "/" + id
+
+	return c.Status(fiber.StatusOK).JSON(resp)
 }
